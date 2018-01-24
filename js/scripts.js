af8d7426fa022d46eccae562344fe9d9fcd86672
Наймушин Егор <webmaster@farexpo.local>
2018-01-24 18:21:41 +0300

    ticket + comments

--- js/scripts.js
+++ js/scripts.js
@@ -20,4 +20,32 @@ $(document).ready(function(){
 			
 	});
 	
+	$('.com-items').slick({
+	  infinite: true,
+	  autoplay: true,
+	  speed: 1500,
+	  slidesToShow: 4,
+	  slidesToScroll: 1,
+	  responsive: [
+		{
+		  breakpoint: 1199,
+		  settings: {
+			slidesToShow: 3,
+		  }
+		},
+		{
+		  breakpoint: 991,
+		  settings: {
+			slidesToShow: 2,
+		  }
+		},
+		{
+		  breakpoint: 680,
+		  settings: {
+			slidesToShow: 1,
+		  }
+		}
+	  ] 
+	});
+	
 });
